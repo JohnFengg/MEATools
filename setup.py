3a129b0ede8ee79c689a59d6dc75f4193a58845b
JohnFengg <147357151+JohnFengg@users.noreply.github.com>
2025-10-23 14:46:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name="meatools",
-    version="0.1.0",
+    version="0.3.2",
     author='Shicheng Xu，Jiangyuan John Feng',
     description='',
     requires=[
