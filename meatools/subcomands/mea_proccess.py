879571e0a39ee48c7fe7489bdae4e4f56c2eeb1a
JohnFengg <fengjiangyuan@outlook.com>
2026-07-05 16:21:24 +0800

    Initial commit: meatools v0.3.5

---
+++ meatools/subcomands/mea_proccess.py
@@ -0,0 +1,45 @@
+import subprocess,sys,os
+
+# def run_evt(args=None):
+#     path=os.path.join(os.path.dirname(__file__),'lasp_train_evt')
+#     subprocess.run(['bash',path])
+
+def run_test_sequence(args=None):
+    path=os.path.join(os.path.dirname(__file__),'test_squence.py')
+    subprocess.run([sys.executable,path])
+
+def run_otr(args=None):
+    path=os.path.join(os.path.dirname(__file__),'impedence_calc.py')
+    subprocess.run([sys.executable,path])
+
+def run_ecsa(args=None):
+    path=os.path.join(os.path.dirname(__file__),'ecsa_normal.py')
+    subprocess.run([sys.executable,path])
+
+def run_ecsa_dry(args=None):
+    path=os.path.join(os.path.dirname(__file__),'ecsa_dry.py')
+    subprocess.run([sys.executable,path])
+
+def run_lsv(args=None):
+    path=os.path.join(os.path.dirname(__file__),'lsv.py')
+    subprocess.run([sys.executable,path])
+
+def run_conclude(args=None):
+    path=os.path.join(os.path.dirname(__file__),'conclude.py')
+    subprocess.run([sys.executable,path])
+
+def run_eis(args=None):
+    path=os.path.join(os.path.dirname(__file__),'eis.py')
+    subprocess.run([sys.executable,path])
+
+
+def run_all(args=None):
+    dirs=[dir for dir in os.listdir() if os.path.isdir(dir)]
+    run_test_sequence()
+    if "OTR" in dirs:
+        run_otr()
+    run_ecsa()
+    run_ecsa_dry()
+    run_lsv()
+    run_eis()
+    run_conclude()
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="meatools",
+    version="0.3.5",
+    author='Jiangyuan John Feng, Shicheng Xu',
+    description='',
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "cantera",
+        "matplotlib",
+        "scipy",
+        "scikit-learn"
+    ],
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "mea = meatools.cli:main"
+        ]
+    }
+)
