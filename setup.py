8c5fd179093b9604c8162ccbe2a80b9fe1b4397d
JohnFengg <fengjiangyuan@outlook.com>
2026-07-14 17:34:25 +0800

    chore: bump version to 1.1.4
    Release after ecsadry folder-search fix (COdesorb None crash).

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
    name="meatools",
-    version="1.1.3",
+    version="1.1.4",
    author='Jiangyuan John Feng, Shicheng Xu',
     description='',
     python_requires=">=3.7",
